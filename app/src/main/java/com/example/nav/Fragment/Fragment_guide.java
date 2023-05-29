894e20268040f46c8bc961945eeff55eae167b51
ummnn <juniorsam@naver.com>
2023-05-29 21:37:41 +0900

    db띄우기

--- app/src/main/java/com/example/nav/Fragment/Fragment_guide.java
+++ app/src/main/java/com/example/nav/Fragment/Fragment_guide.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,13 +21,16 @@ public class Fragment_guide extends Fragment {
     private String TAG = "프래그먼트";
     private String val = ""; // val 변수 선언 및 초기화
 
+    private TextView resultTextView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.i(TAG, "onCreateView");
         view = inflater.inflate(R.layout.frag_guide, container, false);
+        resultTextView = view.findViewById(R.id.result_textview);
 
-
+        getVal();
         return view;
     }
 
@@ -35,16 +39,19 @@ public class Fragment_guide extends Fragment {
         DataBaseHelper dbHelper = new DataBaseHelper(getContext());
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT * FROM recipe_ingredient where RECIPE_ID = 195453",null);
+        Cursor cursor = db.rawQuery("SELECT * FROM social",null);
         //" and name = ?",new String[]{"홍길동"});
         while (cursor.moveToNext())
         {
             val += cursor.getString(2)+", ";
 
         }
+
         //sqlresult.setText("결과: "+ val);
         cursor.close();
         dbHelper.close();
+
+        resultTextView.setText("결과: " + val);
     }
 
 }
