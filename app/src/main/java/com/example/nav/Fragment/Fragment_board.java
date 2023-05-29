53ebc39a227ce8db24de41aefcd8b115615e5c74
MinoC <whalsdn0731@gmail.com>
2023-05-29 23:50:07 +0900

    게시판

--- app/src/main/java/com/example/nav/Fragment/Fragment_board.java
+++ app/src/main/java/com/example/nav/Fragment/Fragment_board.java
@@ -1,10 +1,18 @@
 package com.example.nav.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,18 +20,144 @@ import androidx.fragment.app.Fragment;
 
 import com.example.nav.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class Fragment_board extends Fragment {
 
-    private View view;
-    private String TAG = "프래그먼트";
+    private ListView listView;
+    private Button reg_button;
+    private ArrayList<String> titleList = new ArrayList<>();
+    private ArrayList<String> seqList = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.i(TAG, "onCreateView");
-        view = inflater.inflate(R.layout.frag_board, container, false);
+        View view = inflater.inflate(R.layout.frag_board, container, false);
 
+        listView = view.findViewById(R.id.listView);
+        reg_button = view.findViewById(R.id.reg_button);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), adapterView.getItemAtPosition(i) + " 클릭", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra("board_seq", seqList.get(i));
+                startActivity(intent);
+            }
+        });
+
+        reg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        GetBoard getBoard = new GetBoard();
+        getBoard.execute();
+    }
+
+    private class GetBoard extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d("프래그먼트", "onPreExecute");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("프래그먼트", "onPostExecute, " + result);
+
+            titleList.clear();
+            seqList.clear();
+
+            if (result != null && !result.isEmpty()) {
+                try {
+                    JSONArray jsonArray = new JSONArray(result);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String title = jsonObject.optString("title");
+                        String seq = jsonObject.optString("seq");
+                        titleList.add(title);
+                        seqList.add(seq);
+                    }
+
+                    if (arrayAdapter == null) {
+                        arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, titleList);
+                        listView.setAdapter(arrayAdapter);
+                    } else {
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String server_url = "http://15.164.252.136/load_board.php";
+            URL url;
+            String response = "";
+            try {
+                url = new URL(server_url);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                    }
+                } else {
+                    response = "";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
     }
 }
