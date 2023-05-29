d93ab71f580ee2d47664e74198b7c0605f67d337
ummnn <juniorsam@naver.com>
2023-05-29 20:48:02 +0900

    db추가, 연동

--- app/src/main/java/com/example/nav/Fragment/Fragment_guide.java
+++ app/src/main/java/com/example/nav/Fragment/Fragment_guide.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,7 @@ public class Fragment_guide extends Fragment {
 
     private View view;
     private String TAG = "프래그먼트";
+    private String val = ""; // val 변수 선언 및 초기화
 
     @Nullable
     @Override
@@ -26,4 +29,24 @@ public class Fragment_guide extends Fragment {
 
         return view;
     }
+
+    public void getVal() {
+
+        DataBaseHelper dbHelper = new DataBaseHelper(getContext());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM recipe_ingredient where RECIPE_ID = 195453",null);
+        //" and name = ?",new String[]{"홍길동"});
+        while (cursor.moveToNext())
+        {
+            val += cursor.getString(2)+", ";
+
+        }
+        //sqlresult.setText("결과: "+ val);
+        cursor.close();
+        dbHelper.close();
+    }
+
 }
+
+
