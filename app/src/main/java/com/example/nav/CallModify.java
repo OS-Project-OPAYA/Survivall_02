e1f23e388fd6b573eeb3903d1f457e06cff915d6
kjy <realzero101@gmail.com>
2023-06-06 19:25:20 +0900

    메시지 전송 및 연락처 수정 기능 구현

--- app/src/main/java/com/example/nav/CallModify.java
+++ app/src/main/java/com/example/nav/CallModify.java
@@ -1,16 +1,76 @@
 package com.example.nav;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CallModify extends AppCompatActivity {
+
+    private Map<String, String> contactMap;
+
+    private EditText contact1EditText, contact2EditText, contact3EditText, contact4EditText, contact5EditText, contact6EditText;
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call_modify);
+
+        contactMap = (Map<String, String>) getIntent().getSerializableExtra("contactMap");
+
+        contact1EditText = findViewById(R.id.contact1);
+        contact2EditText = findViewById(R.id.contact2);
+        contact3EditText = findViewById(R.id.contact3);
+        contact4EditText = findViewById(R.id.contact4);
+        contact5EditText = findViewById(R.id.contact5);
+        contact6EditText = findViewById(R.id.contact6);
+
+        confirmButton = findViewById(R.id.modifyupdate);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 수정된 전화번호를 업데이트
+                String updatedContact1 = contact1EditText.getText().toString();
+                String updatedContact2 = contact2EditText.getText().toString();
+                String updatedContact3 = contact3EditText.getText().toString();
+                String updatedContact4 = contact4EditText.getText().toString();
+                String updatedContact5 = contact5EditText.getText().toString();
+                String updatedContact6 = contact6EditText.getText().toString();
+
+                contactMap.put("num1", updatedContact1);
+                contactMap.put("num2", updatedContact2);
+                contactMap.put("num3", updatedContact3);
+                contactMap.put("num4", updatedContact4);
+                contactMap.put("num5", updatedContact5);
+                contactMap.put("num6", updatedContact6);
+
+                // 수정된 전화번호를 저장한 후 액티비티 종료
+                Intent intent = new Intent();
+                intent.putExtra("contactMap", (Serializable) contactMap);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+
+        // Set the initial values for the EditText fields from the contactMap
+        contact1EditText.setText(contactMap.get("num1"));
+        contact2EditText.setText(contactMap.get("num2"));
+        contact3EditText.setText(contactMap.get("num3"));
+        contact4EditText.setText(contactMap.get("num4"));
+        contact5EditText.setText(contactMap.get("num5"));
+        contact6EditText.setText(contactMap.get("num6"));
     }
+
+
 }
--- app/src/main/java/com/example/nav/Fragment/Fragment_call.java
+++ app/src/main/java/com/example/nav/Fragment/Fragment_call.java
@@ -26,6 +26,8 @@ public class Fragment_call extends Fragment {
     private View view;
     private String TAG = "프래그먼트";
 
+    private static final int MODIFY_REQUEST_CODE = 1; // Request code for CallModify activity
+
     private Map<String, String> contactMap; // Map to store contact names and phone numbers
 
     @Nullable
@@ -36,58 +38,64 @@ public class Fragment_call extends Fragment {
 
         // Initialize the contact map
         contactMap = new HashMap<>();
-        contactMap.put("home", "12345");
-        contactMap.put("mom", "11111");
-        contactMap.put("dad", "22222");
-        contactMap.put("minwoo", "33333");
-        contactMap.put("donghyun", "44444");
-        contactMap.put("jinyoung", "55555");
+        contactMap.put("num1", "11111");
+        contactMap.put("num2", "22222");
+        contactMap.put("num3", "33333");
+        contactMap.put("num4", "44444");
+        contactMap.put("num5", "55555");
+        contactMap.put("num6", "66666");
 
-        Button homeButton = view.findViewById(R.id.home_button);
-        homeButton.setOnClickListener(new View.OnClickListener() {
+        Button c1 = view.findViewById(R.id.c1);
+        c1.setText(contactMap.get("num1")); // 저장된 번호로 버튼 텍스트 설정
+        c1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber(contactMap.get("home"));
+                dialPhoneNumber(contactMap.get("num1"));
             }
         });
 
-        Button momButton = view.findViewById(R.id.mom_button);
-        momButton.setOnClickListener(new View.OnClickListener() {
+        Button c2 = view.findViewById(R.id.c2);
+        c2.setText(contactMap.get("num2")); // 저장된 번호로 버튼 텍스트 설정
+        c2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber(contactMap.get("mom"));
+                dialPhoneNumber(contactMap.get("num2"));
             }
         });
 
-        Button dadButton = view.findViewById(R.id.dad_button);
-        dadButton.setOnClickListener(new View.OnClickListener() {
+        Button c3 = view.findViewById(R.id.c3);
+        c3.setText(contactMap.get("num3")); // 저장된 번호로 버튼 텍스트 설정
+        c3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber(contactMap.get("dad"));
+                dialPhoneNumber(contactMap.get("num3"));
             }
         });
 
-        Button minwooButton = view.findViewById(R.id.minwoo_button);
-        minwooButton.setOnClickListener(new View.OnClickListener() {
+        Button c4 = view.findViewById(R.id.c4);
+        c4.setText(contactMap.get("num4")); // 저장된 번호로 버튼 텍스트 설정
+        c4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber(contactMap.get("minwoo"));
+                dialPhoneNumber(contactMap.get("num4"));
             }
         });
 
-        Button donghyunButton = view.findViewById(R.id.donghyun_button);
-        donghyunButton.setOnClickListener(new View.OnClickListener() {
+        Button c5 = view.findViewById(R.id.c5);
+        c5.setText(contactMap.get("num5")); // 저장된 번호로 버튼 텍스트 설정
+        c5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber(contactMap.get("donghyun"));
+                dialPhoneNumber(contactMap.get("num5"));
             }
         });
 
-        Button jinyoungButton = view.findViewById(R.id.jinyoung_button);
-        jinyoungButton.setOnClickListener(new View.OnClickListener() {
+        Button c6 = view.findViewById(R.id.c6);
+        c6.setText(contactMap.get("num6")); // 저장된 번호로 버튼 텍스트 설정
+        c6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialPhoneNumber(contactMap.get("jinyoung"));
+                dialPhoneNumber(contactMap.get("num6"));
             }
         });
 
@@ -104,9 +112,25 @@ public class Fragment_call extends Fragment {
             @Override
             public void onClick(View v) {
                 String message = ((EditText) view.findViewById(R.id.title_et)).getText().toString();
+
+                // 연락처 목록을 문자열로 변환하여 콤마로 구분합니다.
+                StringBuilder phoneNumberListBuilder = new StringBuilder();
                 for (Map.Entry<String, String> entry : contactMap.entrySet()) {
-                    sendMessage(entry.getValue(), message);
+                    String phoneNumber = entry.getValue();
+                    phoneNumberListBuilder.append(phoneNumber).append(",");
                 }
+                String phoneNumberList = phoneNumberListBuilder.toString();
+
+                // 문자열에 마지막 콤마가 있을 경우 제거합니다.
+                if (phoneNumberList.endsWith(",")) {
+                    phoneNumberList = phoneNumberList.substring(0, phoneNumberList.length() - 1);
+                }
+
+                // 여러 연락처에 동시에 메시지를 보내기 위해 ACTION_SENDTO를 사용합니다.
+                Uri uri = Uri.parse("smsto:" + phoneNumberList);
+                Intent sendIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                sendIntent.putExtra("sms_body", message);
+                startActivity(sendIntent);
             }
         });
 
@@ -119,13 +143,40 @@ public class Fragment_call extends Fragment {
         startActivity(dialIntent);
     }
 
-    private void sendMessage(String phoneNumber, String message) {
-        // Implement your logic to send the message to the given phone number
-    }
-
     private void openEditContactsActivity() {
         Intent intent = new Intent(getActivity(), CallModify.class);
         intent.putExtra("contactMap", (Serializable) contactMap);
-        startActivity(intent);
+        startActivityForResult(intent, MODIFY_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MODIFY_REQUEST_CODE && resultCode == getActivity().RESULT_OK) {
+            Map<String, String> updatedContactMap = (Map<String, String>) data.getSerializableExtra("contactMap");
+            if (updatedContactMap != null) {
+                contactMap = updatedContactMap;
+                // 수정된 연락처를 반영하는 작업 수행
+                // 예: 버튼의 텍스트를 수정된 연락처로 업데이트
+                Button c1 = view.findViewById(R.id.c1);
+                c1.setText(contactMap.get("num1"));
+
+                Button c2 = view.findViewById(R.id.c2);
+                c2.setText(contactMap.get("num2"));
+
+                Button c3  = view.findViewById(R.id.c3);
+                c3.setText(contactMap.get("num3"));
+
+                Button c4 = view.findViewById(R.id.c4);
+                c4.setText(contactMap.get("num4"));
+
+                Button c5 = view.findViewById(R.id.c5);
+                c5.setText(contactMap.get("num5"));
+
+                Button c6 = view.findViewById(R.id.c6);
+                c6.setText(contactMap.get("num6"));
+            }
+        }
     }
 }
