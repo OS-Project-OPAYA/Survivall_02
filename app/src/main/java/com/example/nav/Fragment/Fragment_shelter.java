948cdff65abe97892ff5c2db6bc51814ff9ccddb
kjy <realzero101@gmail.com>
2023-06-06 20:48:43 +0900

    지도 기능 구현

--- app/src/main/java/com/example/nav/Fragment/Fragment_shelter.java
+++ app/src/main/java/com/example/nav/Fragment/Fragment_shelter.java
@@ -1,30 +1,88 @@
 package com.example.nav.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.nav.R;
 
-
 public class Fragment_shelter extends Fragment {
     private View view;
     private String TAG = "프래그먼트";
+
+    private GeolocationPermissions.Callback geolocationCallback;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.i(TAG, "onCreateView");
         view = inflater.inflate(R.layout.frag_shelter, container, false);
+
+        WebView webView = view.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                geolocationCallback = callback;
+
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(requireActivity(),
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            LOCATION_PERMISSION_REQUEST_CODE);
+                } else {
+                    geolocationCallback.invoke(origin, true, false);
+                }
+            }
+
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                super.onPermissionRequest(request);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
+        });
+
+        webView.loadUrl("https://m.map.kakao.com/actions/searchView?q=대피소&wxEnc=NOLMLM&wyEnc=XQVPTP&lvl=7&viewmap=true");
+
         return view;
     }
 
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (geolocationCallback != null) {
+                    geolocationCallback.invoke("", true, false);
+                }
+            } else {
+                // 위치 권한이 거부되었을 경우 처리
+            }
+        }
+    }
 }
